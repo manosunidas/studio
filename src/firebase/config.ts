46549d02a753280a827f8c57a16b5349c733a2b0
Jhelen Andrea <jhelenandreat@gmail.com>
2025-10-20 15:42:02 +0000

    MIRA LOS DATOS DE LA APP: // Your web app's Firebase configuration const

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-1933739816-d9066",
-  "appId": "1:644114119677:web:61e9a55bcf49189ba97794",
-  "apiKey": "AIzaSyDxZLymhfGGK7ddMMKw8yEFO73ytiPAW1U",
-  "authDomain": "studio-1933739816-d9066.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "644114119677"
+  apiKey: "AIzaSyDxZLymhfGGK7ddMMKw8yEFO73ytiPAW1U",
+  authDomain: "studio-1933739816-d9066.firebaseapp.com",
+  projectId: "studio-1933739816-d9066",
+  storageBucket: "studio-1933739816-d9066.firebasestorage.app",
+  messagingSenderId: "644114119677",
+  appId: "1:644114119677:web:61e9a55bcf49189ba97794"
 };
