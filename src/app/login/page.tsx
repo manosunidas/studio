0dcbf8e230eba7b6ee3ddf98a2d509191b316562
Jhelen Andrea <jhelenandreat@gmail.com>
2025-10-21 01:15:56 +0000

    HABILITA CAMBIAR DE USUARIO PARA QUE EL USUARIO PUEDA LOGUEARSE CON UNA

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -41,6 +41,9 @@ export default function LoginPage() {
 
   const handleGoogleLogin = async () => {
     const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: 'select_account'
+    });
     try {
       const result = await signInWithPopup(auth, provider);
       const signedInUser = result.user;
@@ -89,5 +92,3 @@ export default function LoginPage() {
     </div>
   );
 }
-
-    
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -19,6 +19,9 @@ export default function RegisterPage() {
   const handleGoogleLogin = async () => {
     const auth = getAuth();
     const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: 'select_account'
+    });
 
     try {
       await signInWithPopup(auth, provider);
